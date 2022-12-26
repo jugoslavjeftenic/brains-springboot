77b5cc1dec27e43689eea70fca33f193226bc255
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2022-12-27 00:58:22 +0100

    Zavrsena getDB() metoda i getAllOffesrs() endpoint.

--- be_t2_02_project/src/main/java/com/iktprekvalifikacija/project/controllers/OfferController.java
+++ be_t2_02_project/src/main/java/com/iktprekvalifikacija/project/controllers/OfferController.java
@@ -4,9 +4,17 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.iktprekvalifikacija.project.entities.CategoryEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.iktprekvalifikacija.project.entities.EOfferEntity;
 import com.iktprekvalifikacija.project.entities.OfferEntity;
 
+import tools.RADE;
+
+@RestController
+@RequestMapping(value = "/project/offers")
 public class OfferController {
 
 	// 3.2
@@ -20,15 +28,40 @@ public class OfferController {
 	private List<OfferEntity> getDB() {
 		if(offers.size() == 0) {
 			oid = 0;
-			String[][] offDesc = {
-					{"Stemovanje zidova u stanovima", "Nepropustite sansu da iznervirate komsije"},
-					{"Automobili", "Brednirajte Vaš službeni automobil ili pronađite agencije koje se bave iznajmljivanjem vozila."}
+			// TODO Umesto string matrice napraviti listu objekata
+			String[][] offProperties = {
+					{"Stemovanje zidova", "Najbolji način da iznervirate komsije.", "500.00", "3000.00"},
+					{"Sportski tipovi", "100% pogoci, analitička firma Zeleni i Kum.", "2000", "100000"},
+					{"Kupon+ aplikacija", "Uštedite sada kupujući stvari koje će vam jednog dana sigurno zatrebati.", "999.99", "999.99"},
+					{"Magazin Zdravo telo u zdravoj fotelji", "Zamenite vežbanje gledanjem slika. Uz svaki primerak magazina dobijate poster fotelje.", "500", "500"},
+					{"Sredstvo za odmagljivanje", "Naučnici su dokazali da je naš prozvod 12% bolji od nekih drugih proizvoda.", "2599.99", "2599.99"},
+					{"Jučerašnji hleb", "Akcijski proizvodi pekare SZR Bred i Pita.", "50", "50"}
 			};
-			for (int i = 0; i < offDesc.length; i++) {
-				offers.add(new OfferEntity(++oid, offDesc[i][0], offDesc[i][1], null, null, null, null, null, i, i, null));
+			for (int i = 0; i < offProperties.length; i++) {
+				LocalDateTime rndOfferStart = LocalDateTime.now().minusHours(RADE.mrRobot(0, 24));
+				Float rndPrice;
+				try {
+					rndPrice = (float) RADE.mrRobot(Float.parseFloat(offProperties[i][2]), Float.parseFloat(offProperties[i][3]));
+				} catch (Exception e) {
+					rndPrice = 1F;
+				}
+				EOfferEntity offerStatus = EOfferEntity.values()[RADE.mrRobot(0, EOfferEntity.values().length - 1)];
+				offers.add(new OfferEntity(++oid, offProperties[i][0], offProperties[i][1],
+						rndOfferStart, rndOfferStart.plusDays(RADE.mrRobot(7, 30)),
+						rndPrice, (float) (rndPrice - (Math.round((rndPrice * RADE.mrRobot(0.05, 0.5))))),
+						"", RADE.mrRobot(10, 1000), RADE.mrRobot(10, 1000), offerStatus));
 			}
 		}
 		return offers;
+	}
+
+	// 3.3
+	/*
+	 * kreirati REST endpoint koja vraća listu svih ponuda • putanja /project/offers
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public List<OfferEntity> getAllOffers() {
+		return getDB();
 	}
 
 }
