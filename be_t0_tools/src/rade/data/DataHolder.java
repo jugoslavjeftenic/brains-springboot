3011b5f155abb6c9c708896789d99b5c13d888cc
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-06 15:32:53 +0100

    Dodata nova zamlja u DataHolder

--- be_t0_tools/src/rade/data/DataHolder.java
+++ be_t0_tools/src/rade/data/DataHolder.java
@@ -8,6 +8,7 @@ public class DataHolder {
 			{"70", "BA", "BIH", "Bosna i Hercegovina", "Evropa"},
 			{"191", "HR", "HRV", "Hrvatska", "Evropa"},
 			{"499", "ME", "MNE", "Crna Gora", "Evropa"},
+			{"533", "AW", "ABW", "Aruba", "South America"},
 			{"688", "RS", "SRB", "Srbija", "Evropa"},
 			{"705", "SI", "SVN", "Slovenija", "Evropa"},
 			{"807", "MK", "MKD", "Republika Makedonija", "Evropa"}
