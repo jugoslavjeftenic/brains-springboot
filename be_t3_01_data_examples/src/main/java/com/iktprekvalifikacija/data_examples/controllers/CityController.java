d4c42fb32588865608d1b14addd796144b3064c3
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-06 05:58:00 +0100

    Zapocet CityController.

--- be_t3_01_data_examples/src/main/java/com/iktprekvalifikacija/data_examples/controllers/CityController.java
+++ be_t3_01_data_examples/src/main/java/com/iktprekvalifikacija/data_examples/controllers/CityController.java
@@ -1,5 +1,8 @@
 package com.iktprekvalifikacija.data_examples.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.iktprekvalifikacija.data_examples.entities.CityEntity;
 import com.iktprekvalifikacija.data_examples.repositories.CityRepository;
+
+import rade.RADE;
 
 @RestController
 @RequestMapping(path = "/api/v1/cities")
@@ -31,17 +36,24 @@ public class CityController {
 		}
 	}
 	
-//	@RequestMapping(method = RequestMethod.POST, path = "/populatetable")
-//	public Iterable<CityEntity> populateTable() {
-//		List<CityEntity> countries = new ArrayList<>();
-//		for (int i = 0; i < RADE.listajSveZemlje().size(); i++) {
-//			if (cityRepository.findByCountry(RADE.listajSveZemlje().get(i).getNaziv()).size() < 1) {
-//				CountryEntity country = new CountryEntity();
-//				country.setCountry(RADE.listajSveZemlje().get(i).getNaziv());
-//				countries.add(country);
-//			}
-//		}
-//		cityRepository.saveAll(countries);
-//		return countries;
-//	}
+	@RequestMapping(method = RequestMethod.POST, path = "/populatetable")
+	public Iterable<CityEntity> populateTable() {
+		List<CityEntity> cities = new ArrayList<>();
+		for (int i = 0; i < RADE.listajSveOpstine().size(); i++) {
+			if (cityRepository.findByCity(RADE.listajSveOpstine().get(i).getNaziv()).size() < 1) {
+				CityEntity city = new CityEntity();
+				city.setCity(RADE.listajSveOpstine().get(i).getNaziv());
+				if (RADE.listajSveOpstine().get(i).getDrzava() != null) {
+					// TODO treba zavrsiti
+					city.setCountry(null);
+				}
+				else {
+					city.setCountry(null);
+				}
+				cities.add(city);
+			}
+		}
+		cityRepository.saveAll(cities);
+		return cities;
+	}
 }
--- be_t3_01_data_examples/src/main/java/com/iktprekvalifikacija/data_examples/repositories/CityRepository.java
+++ be_t3_01_data_examples/src/main/java/com/iktprekvalifikacija/data_examples/repositories/CityRepository.java
@@ -1,9 +1,12 @@
 package com.iktprekvalifikacija.data_examples.repositories;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.iktprekvalifikacija.data_examples.entities.CityEntity;
 
 public interface CityRepository extends CrudRepository<CityEntity, Integer> {
+	List<CityEntity> findByCity(String city);
 
 }
