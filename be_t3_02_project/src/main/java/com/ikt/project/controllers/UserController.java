662b1b578f9e70cd3244e1979dd1d9e23556b9a8
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-13 21:26:06 +0100

    Nastavljena izmena UserController-a za rad sa bazom.

--- be_t3_02_project/src/main/java/com/ikt/project/controllers/UserController.java
+++ be_t3_02_project/src/main/java/com/ikt/project/controllers/UserController.java
@@ -1,6 +1,9 @@
 package com.ikt.project.controllers;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,47 +18,6 @@ import com.ikt.project.repositories.UserRepository;
 @RestController
 @RequestMapping(value = "/api/v1/users")
 public class UserController {
-	
-	// T2 1.2
-	/*
-	 * U paketu com.iktpreobuka.project.controllers napraviti klasu UserController
-	 * sa metodom getDB() koja vraća listu svih korisnika aplikacije
-
-	List<UserEntity> users = new ArrayList<>();
-
-	private List<UserEntity> getDB() {
-		if(users.size() == 0) {
-			uid = 0;
-			users.add(generateUser(0));
-			users.add(generateUser(1));
-			users.add(generateUser(1));
-			users.add(generateUser(2));
-			users.add(generateUser(2));
-			users.add(generateUser(2));
-			users.add(generateUser(2));
-		}
-		return users;
-	}
-
-	private UserEntity generateUser(int userRoleIndex) {
-		String firstName = RADE.generisiIme(0);
-		String lastName = RADE.generisiPrezime();
-		EUserRole userRole;
-		switch(userRoleIndex) {
-	        case 0: userRole = EUserRole.ROLE_ADMIN;
-	        break;
-	        case 1: userRole = EUserRole.ROLE_SELLER;
-	        break;
-	        default: userRole = EUserRole.ROLE_CUSTOMER;
-		}
-		UserEntity client = new UserEntity(
-			++uid, firstName, lastName,
-			firstName.toLowerCase() + lastName.substring(0, 2).toLowerCase(), "pass123",
-			firstName.toLowerCase() + "." + lastName.substring(0, 1).toLowerCase() + "@bomba.net",
-			userRole);
-		return client;
-	}
-	*/
 	
 	// T3 1.3
 	/*
@@ -80,13 +42,6 @@ public class UserController {
 		return user;
 	}
 
-	// T2 1.5
-	/*
-	 * Kreirati REST endpoint koji omogućava dodavanje novog korisnika
-	 * • putanja /project/users
-	 * • u okviru ove metode postavi vrednost atributa user role na ROLE_CUSTOMER
-	 * • metoda treba da vrati dodatog korisnika
-	 */
 	@RequestMapping(method = RequestMethod.POST)
 	public UserEntity addUser(@RequestBody UserEntity newUser) {
 		UserEntity user = new UserEntity();
@@ -100,31 +55,31 @@ public class UserController {
 	}
 
 	// TODO zavrsiti
-//	@RequestMapping(method = RequestMethod.POST, path = "/populatetable/{count}")
-//	public Iterable<UserEntity> populateTable(@PathVariable Integer count) {
-//		List<UserEntity> users = new ArrayList<>();
-//		for (int x = 0; x < count; x++) {
-//			UserEntity user = new UserEntity();
-//			String name = RADE.generisiIme(0) + " " + RADE.generisiPrezime(); 
-//			user.setName(name);
-//			user.setEmail(name.substring(0, name.indexOf(' ')).toLowerCase() + "."
-//					+ name.substring(name.indexOf(' ') + 1, name.indexOf(' ') + 2).toLowerCase()
-//					+ "@ikt.rs");
-//			user.setJmbg(RADE.generisiJMBG());
-//			user.setBirthDate(LocalDate.parse(
-//					((Integer.parseInt(user.getJmbg().substring(4, 7)) > 900) ? "1" : "2") +
-//							user.getJmbg().substring(4, 7) +
-//							user.getJmbg().substring(2, 4) +
-//							user.getJmbg().substring(0, 2),
-//					DateTimeFormatter.BASIC_ISO_DATE));
-//			user.setPhoneNumber(String.format("%3s", RADE.mrRobot(10, 37)).replace(" ", "0") + "/" +
-//					String.format("%3s", RADE.mrRobot(0, 999)).replace(" ", "0") + "-" +
-//					String.format("%3s", RADE.mrRobot(0, 9999)).replace(" ", "0"));
-//			user.setRegBrLk(String.format("%9s", RADE.mrRobot(1, 999999999)).replace(" ", "0"));
-//			users.add(user);
-//		}
-//		return userRepository.saveAll(users);
-//	}
+	@RequestMapping(method = RequestMethod.POST, path = "/populatetable/{count}")
+	public Iterable<UserEntity> populateTable(@PathVariable Integer count) {
+		List<UserEntity> users = new ArrayList<>();
+		for (int x = 0; x < count; x++) {
+			UserEntity user = new UserEntity();
+			String name = RADE.generisiIme(0) + " " + RADE.generisiPrezime(); 
+			user.setName(name);
+			user.setEmail(name.substring(0, name.indexOf(' ')).toLowerCase() + "."
+					+ name.substring(name.indexOf(' ') + 1, name.indexOf(' ') + 2).toLowerCase()
+					+ "@ikt.rs");
+			user.setJmbg(RADE.generisiJMBG());
+			user.setBirthDate(LocalDate.parse(
+					((Integer.parseInt(user.getJmbg().substring(4, 7)) > 900) ? "1" : "2") +
+							user.getJmbg().substring(4, 7) +
+							user.getJmbg().substring(2, 4) +
+							user.getJmbg().substring(0, 2),
+					DateTimeFormatter.BASIC_ISO_DATE));
+			user.setPhoneNumber(String.format("%3s", RADE.mrRobot(10, 37)).replace(" ", "0") + "/" +
+					String.format("%3s", RADE.mrRobot(0, 999)).replace(" ", "0") + "-" +
+					String.format("%3s", RADE.mrRobot(0, 9999)).replace(" ", "0"));
+			user.setRegBrLk(String.format("%9s", RADE.mrRobot(1, 999999999)).replace(" ", "0"));
+			users.add(user);
+		}
+		return userRepository.saveAll(users);
+	}
 
 	
 	// T2 1.6
