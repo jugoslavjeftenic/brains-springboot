3c5a107a5b9aea84abd4e921239d24f5bcdd6fc6
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-16 21:08:07 +0100

    Otvoren prazan T4 projekat.

---
+++ be_t4_04_project/src/test/java/com/ikt/t4/project/BeT404ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikt.t4.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeT404ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
