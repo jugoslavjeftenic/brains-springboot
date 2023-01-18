fbc5917e06a89426cd46f35a19d356aed40aa829
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-19 00:03:50 +0100

    Zapocet T3 Zadatak 4.6 - Voucher CRUD operacije.

--- be_t3_02_project/src/main/java/com/ikt/t3/project/controllers/VoucherController.java
+++ be_t3_02_project/src/main/java/com/ikt/t3/project/controllers/VoucherController.java
@@ -1,11 +1,18 @@
 package com.ikt.t3.project.controllers;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ikt.t3.project.entites.VoucherEntity;
+import com.ikt.t3.project.repositories.OfferRepository;
+import com.ikt.t3.project.repositories.UserRepository;
 import com.ikt.t3.project.repositories.VoucherRepository;
 
 @RestController
@@ -20,10 +27,27 @@ public class VoucherController {
 	 */
 	@Autowired
 	private VoucherRepository voucherRepository;
-	
+
+	@Autowired
+	private OfferRepository offerRepository;
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public Iterable<VoucherEntity> getAll() {
 		return voucherRepository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.GET, path = "/{id}")
+	public VoucherEntity getById(@PathVariable Integer id) {
+		VoucherEntity voucher;
+		try {
+			voucher = voucherRepository.findById(id).get();
+			return voucher;
+		} catch (Exception e) {
+			return null;
+		}
 	}
 	
 	// T3 4.6
@@ -35,5 +59,32 @@ public class VoucherController {
 	 * • putanja /project/vouchers/{id} (izmena)
 	 * • putanja /project/vouchers/{id} (brisanje)
 	 */
-	
+
+	// TODO testirati
+	@RequestMapping(method = RequestMethod.POST, path = "/{offerId}/buyer/{buyerId}")
+	public VoucherEntity addVoucher(@PathVariable Integer offerId, @PathVariable Integer buyerId,
+			@RequestParam String expirationDate, @RequestParam Boolean isUsed) {
+		DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+		LocalDateTime exDate;
+		try {
+			exDate = LocalDateTime.parse(expirationDate, formatter);
+		} catch (Exception e) {
+			try {
+				exDate = LocalDateTime.parse(expirationDate + "T00:00:00", formatter);
+				exDate = exDate.plusDays(1);
+			} catch (Exception e2) {
+				return null;
+			}
+		}
+		try {
+			VoucherEntity newVoucher = new VoucherEntity();
+			newVoucher.setExpirationDate(exDate);
+			newVoucher.setIsUsed(isUsed);
+			newVoucher.setOffer(offerRepository.findById(offerId).get());
+			newVoucher.setUser(userRepository.findById(buyerId).get());
+			return voucherRepository.save(newVoucher);
+		} catch (Exception e) {
+			return null;
+		}
+	}
 }
