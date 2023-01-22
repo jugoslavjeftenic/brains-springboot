64aa6a98c911b59c3c7dbda8aad6ddcec22a380f
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-22 13:38:59 +0100

    Preimenovana Application klasa.

--- be_t2_01_example_rest/src/main/java/com/ikt/t2/example_rest/BeT201ExampleRestApplication.java
+++ be_t2_01_example_rest/src/main/java/com/ikt/t2/example_rest/BeT201ExampleRestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BE_T2_01_RestExamplesApplication {
+public class BeT201ExampleRestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BE_T2_01_RestExamplesApplication.class, args);
+		SpringApplication.run(BeT201ExampleRestApplication.class, args);
 	}
 
 }
