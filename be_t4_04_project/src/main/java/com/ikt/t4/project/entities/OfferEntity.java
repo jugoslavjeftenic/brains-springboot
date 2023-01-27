9b5e8dad95b8a4d9214d31f12dcc7b2c4b5b4074
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-27 21:27:38 +0100

    Problem soft delet-a resen anotacijom @NotFound(ima nove probleme).

--- be_t4_04_project/src/main/java/com/ikt/t4/project/entities/OfferEntity.java
+++ be_t4_04_project/src/main/java/com/ikt/t4/project/entities/OfferEntity.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.NotFound;
+import org.hibernate.annotations.NotFoundAction;
 import org.hibernate.annotations.ResultCheckStyle;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.Where;
@@ -59,9 +61,12 @@ public class OfferEntity {
 	@Column(nullable=false)
 	private boolean deleted = Boolean.FALSE;
 
+	// https://www.jpa-buddy.com/blog/soft-deletion-in-hibernate-things-you-may-miss/
+	@NotFound(action = NotFoundAction.IGNORE)
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
 	@JoinColumn(name = "category")
 	private CategoryEntity category;
+	@NotFound(action = NotFoundAction.IGNORE)
 	@ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH)
 	@JoinColumn(name = "user")
 	private UserEntity user;
