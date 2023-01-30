5c5fff2adb4d4acfcc913ec9d624028c820f37c7
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-30 10:25:10 +0100

    Otvoren prazan projekat za danasnje vezbe.

---
+++ be_t7_01_example_security/src/main/java/com/ikt/t7/example_security/BeT701ExampleSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.ikt.t7.example_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeT701ExampleSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeT701ExampleSecurityApplication.class, args);
+	}
+
+}
