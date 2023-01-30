4cab4e3b05fbe59c3b1475c0bfa538c718a8f6e3
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-01-30 10:25:47 +0100

    Otvoren prazan projekat za danasnje vezbe.

---
+++ be_t7_01_example_security/src/test/java/com/ikt/t71/BeT701ExampleSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikt.t71;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeT701ExampleSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
