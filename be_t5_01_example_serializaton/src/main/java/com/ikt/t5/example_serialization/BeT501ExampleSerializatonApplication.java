e175ebbe3ca832dfaa219b2047c27b1a9c1c931e
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-02-03 23:40:37 +0100

    Uradjen prvi deo vezbi T5 termina. Serijalizacija i pogledi.

---
+++ be_t5_01_example_serializaton/src/main/java/com/ikt/t5/example_serialization/BeT501ExampleSerializatonApplication.java
@@ -0,0 +1,13 @@
+package com.ikt.t5.example_serialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeT501ExampleSerializatonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeT501ExampleSerializatonApplication.class, args);
+	}
+
+}
---
+++ be_t5_01_example_serializaton/src/test/java/com/ikt/t5/example_serialization/BeT501ExampleSerializatonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikt.t5.example_serialization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeT501ExampleSerializatonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
