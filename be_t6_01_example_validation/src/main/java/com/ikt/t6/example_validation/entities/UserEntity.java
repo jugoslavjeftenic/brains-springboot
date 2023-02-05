72367f3e7d935048384c1989f31910b9a0b51c49
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-02-05 10:36:52 +0100

    Otvoren novi projekat za T6 vezbe.

---
+++ be_t6_01_example_validation/src/main/java/com/ikt/t6/example_validation/entities/UserEntity.java
@@ -0,0 +1,108 @@
+package com.ikt.t6.example_validation.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "users_generator")
+	@SequenceGenerator(name="users_generator", sequenceName = "users_sequence", allocationSize=1)
+	private Integer id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String username;
+	private String password;
+	private Integer age;
+	@Version
+	private Integer version;
+
+	public UserEntity() {
+		super();
+	}
+
+	public UserEntity(Integer id, String firstName, String lastName, String email, String username, String password,
+			Integer age, Integer version) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.age = age;
+		this.version = version;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+}
