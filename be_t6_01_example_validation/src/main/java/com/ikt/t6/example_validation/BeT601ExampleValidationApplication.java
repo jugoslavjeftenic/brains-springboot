e6efe4a633a4941c6f8733e076fab6c5eb3c1c1c
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-02-05 10:37:34 +0100

    Otvoren novi projekat za T6 vezbe.

---
+++ be_t6_01_example_validation/src/main/java/com/ikt/t6/example_validation/BeT601ExampleValidationApplication.java
@@ -0,0 +1,13 @@
+package com.ikt.t6.example_validation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeT601ExampleValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeT601ExampleValidationApplication.class, args);
+	}
+
+}
---
+++ be_t6_01_example_validation/src/test/java/com/ikt/t6/example_validation/BeT601ExampleValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikt.t6.example_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeT601ExampleValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
