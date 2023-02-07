d4b02f059a8e6740c94a54bd41d1fc3bcfbe901d
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-02-07 10:56:28 +0100

    Otvoren novi projekat za T7 vezbe basic auth.

---
+++ be_t7_01_example_basic_auth/src/main/java/com/ikt/t7/example_basic_auth/BeT701ExampleBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ikt.t7.example_basic_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeT701ExampleBasicAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeT701ExampleBasicAuthApplication.class, args);
+	}
+
+}
