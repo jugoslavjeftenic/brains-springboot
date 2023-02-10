fd960b53f4b35c520bc3a3669255eb7c8043e224
jugoslavjeftenic <jugoslav.jeftenic.su@gmail.com>
2023-02-10 13:44:44 +0100

    Nastavak T7 vezbe token auth.

---
+++ be_t7_02_example_token_auth/src/main/java/com/ikt/t7/example_token_auth/security/config/JWTAuthorizationFilter.java
@@ -0,0 +1,78 @@
+package com.ikt.t7.example_token_auth.security.config;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.crypto.SecretKey;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+
+	private final String HEADER = "Authorization";
+	private final String PREFIX = "Bearer ";
+	
+	private SecretKey secretKey;
+	
+	public JWTAuthorizationFilter(SecretKey secretKey) {
+		super();
+		this.secretKey = secretKey;
+	}
+
+	private boolean checkIfJWTExist(HttpServletRequest request) {
+		String authorizationHeaderValue = request.getHeader(HEADER);
+//		if (authorizationHeaderValue == null || !authorizationHeaderValue.startsWith("Bearer ")) {
+//			return false;
+//		}
+//		return true;
+		return !(authorizationHeaderValue == null || !authorizationHeaderValue.startsWith(PREFIX));
+	}
+	
+	private Claims validateToken(HttpServletRequest request) {
+		String jwt = request.getHeader(HEADER).replace(PREFIX, "");
+		return Jwts.parserBuilder().setSigningKey(this.secretKey).build().parseClaimsJws(jwt).getBody();
+	}
+	
+	private void setUpSpringAuthentication(Claims claims) {
+		@SuppressWarnings("unchecked")
+		List<String> authorities = (List<String>) claims.get("authorities");
+		UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(
+				claims.getSubject(),
+				null,
+				authorities
+					.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList())
+		);
+		SecurityContextHolder.getContext().setAuthentication(auth);
+	}
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		if (checkIfJWTExist(request)) {
+			Claims claims = validateToken(request);
+			if (claims.get("authorities") != null) {
+				setUpSpringAuthentication(claims);
+			}
+			else {
+				SecurityContextHolder.clearContext();
+			}
+		}
+		else {
+			SecurityContextHolder.clearContext();
+		}
+		filterChain.doFilter(request, response);
+	}
+}
---
+++ be_t7_02_example_token_auth/src/main/java/com/ikt/t7/example_token_auth/security/config/WebSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.ikt.t7.example_token_auth.security.config;
+
+import javax.crypto.SecretKey;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig {
+
+	private SecretKey secretKey;
+	
+	public WebSecurityConfig() {
+		this.secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+	}
+	
+	@Bean
+	SecretKey secretKey() {
+		return this.secretKey;
+	}
+	
+	@Bean
+	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity
+			.csrf().disable()
+			.addFilterAfter(new JWTAuthorizationFilter(secretKey), UsernamePasswordAuthenticationFilter.class)
+			.authorizeRequests().antMatchers(HttpMethod.POST, "/api/v1/login").permitAll()
+			.anyRequest().authenticated();
+		return httpSecurity.build();
+	}
+}
