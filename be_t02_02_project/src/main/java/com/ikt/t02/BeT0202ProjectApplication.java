af90aebbd58436d79ce7a769cdb7612a3090cc3c
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-11 11:12:25 +0100

    Preimenovan projekat.

---
+++ be_t02_02_project/src/main/java/com/ikt/t02/BeT0202ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ikt.t02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeT0202ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeT0202ProjectApplication.class, args);
+	}
+
+}
---
+++ be_t02_02_project/src/test/java/com/ikt/t02/BeT0202ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikt.t02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeT0202ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
