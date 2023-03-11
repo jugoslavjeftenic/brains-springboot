026cc190386b2898e12410575ebc104c32426a6a
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-11 09:37:03 +0100

    Zavrsni BE projekat.

---
+++ be_t99_back_to_school/src/main/java/com/ikt/t99/BeT99BackToSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.ikt.t99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeT99BackToSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeT99BackToSchoolApplication.class, args);
+	}
+
+}
---
+++ be_t99_back_to_school/src/test/java/com/ikt/t99/BeT99BackToSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikt.t99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeT99BackToSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
