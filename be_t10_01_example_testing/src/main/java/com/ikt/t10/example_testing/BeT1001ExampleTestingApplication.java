f8e6357da15671c2dc399edf24548817475ba895
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-12 06:48:38 +0100

    Zapocete T10 vezbe - testiranje.

---
+++ be_t10_01_example_testing/src/main/java/com/ikt/t10/example_testing/BeT1001ExampleTestingApplication.java
@@ -0,0 +1,13 @@
+package com.ikt.t10.example_testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeT1001ExampleTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeT1001ExampleTestingApplication.class, args);
+	}
+
+}
---
+++ be_t10_01_example_testing/src/test/java/com/ikt/t10/example_testing/BeT1001ExampleTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikt.t10.example_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeT1001ExampleTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
