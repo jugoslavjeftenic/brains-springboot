04c618692ae75be284c1b5a2fb7da9e9ab93134d
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2023-03-12 06:57:52 +0100

    Nastavljena T10 vezba - testiranje.

---
+++ be_t10_01_example_testing/src/main/java/com/ikt/t10/example_testing/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.ikt.t10.example_testing.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String greeting() {
+		return "Hello World";
+	}
+}
